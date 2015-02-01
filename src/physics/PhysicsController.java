7ae47f745a704ef60bc3ee729e95d96c8481a1dc
firefly431 <firefly431.awesome@gmail.com>
2015-01-31 19:57:19 -0600

    Add separation to PhysicsController
    I think this was the only thing I did; it's been awhile since I last
    remember committing.

--- src/physics/PhysicsController.java
+++ src/physics/PhysicsController.java
@@ -36,8 +36,25 @@ public class PhysicsController {
         });
         for (UnorderedPair<PhysicsObject> pair : collidingObjects) {
             Collision col = Collision.resolve(pair);
-            if (col != null)
+            if (col != null && col.valid)
                 collisions.add(col);
+            else if (col != null) {
+                // invalid collision, try to separate
+                PhysicsObject A = col.objects.getA(), B = col.objects.getB();
+                if (!(A instanceof MovingObject)) {
+                    if (!(B instanceof MovingObject)) {
+                        // do nothing
+                    } else {
+                        separateMoving1((MovingObject) B, A, col);
+                    }
+                } else {
+                    if (!(B instanceof MovingObject)) {
+                        separateMoving1((MovingObject) A, B, col);
+                    } else {
+                        separateMoving2((MovingObject) A, (MovingObject) B, col);
+                    }
+                }
+            }
         }
         HashSet<PhysicsObject> seen = new HashSet<>();
         for (Collision collision : collisions) {
@@ -50,20 +67,20 @@ public class PhysicsController {
                 } else {
                     if (seen.contains(B))
                         continue;
-                    doCollisionMoving1((MovingObject) B, A);
+                    doCollisionMoving1((MovingObject) B, A, collision);
                     seen.add(B);
                     continue;
                 }
             } else if (!(B instanceof MovingObject)) {
                 if (seen.contains(A))
                     continue;
-                doCollisionMoving1((MovingObject) A, B);
+                doCollisionMoving1((MovingObject) A, B, collision);
                 seen.add(A);
                 continue;
             }
             if (seen.contains(A) || seen.contains(B))
                 continue;
-            doCollisionMoving2((MovingObject) A, (MovingObject) B);
+            doCollisionMoving2((MovingObject) A, (MovingObject) B, collision);
             seen.add(A);
             seen.add(B);
         }
@@ -76,17 +93,44 @@ public class PhysicsController {
         }
     }
 
+    private void separateMoving2(MovingObject A, MovingObject B, Collision collision) {
+        // know that normal is pointing toward A
+    }
+
+    private void separateMoving1(MovingObject A, PhysicsObject B, Collision collision) {
+        AxisAlignedBoundingBox bA = A.getAxisAlignedBoundingBox(),
+                               bB = B.getAxisAlignedBoundingBox();
+        System.out.println(collision.normal);
+        Vector delta = new Vector();
+        if (collision.normal.x < 0) {
+            delta.x = bB.x1 - bA.x2;
+        } else if (collision.normal.x > 0) {
+            delta.x = bB.x2 - bA.x1;
+        }
+        if (collision.normal.y < 0) {
+            delta.y = bB.y1 - bA.y2;
+        } else if (collision.normal.y > 0) {
+            delta.y = bB.y2 - bA.y1;
+        }
+        System.out.println("Separated by " + delta.magnitude());
+        A.setPosition(A.getPosition().plus(delta));
+    }
+
 
     // moving and static
-    private void doCollisionMoving1(MovingObject a, PhysicsObject b) {
+    private void doCollisionMoving1(MovingObject a, PhysicsObject b,
+                                    Collision collision) {
         a.collide(b);
         b.collide(a);
-        // stub
+        // TODO: real code
         System.out.println("Collision!");
+        a.setPosition(a.getPosition().plus(a.getVelocity().times(collision.time)));
+        a.setVelocity(new Vector());
     }
 
     // moving and moving
-    private void doCollisionMoving2(MovingObject a, MovingObject b) {
+    private void doCollisionMoving2(MovingObject a, MovingObject b,
+                                    Collision collision) {
         a.collide(b);
         b.collide(a);
         // stub
@@ -96,11 +140,15 @@ public class PhysicsController {
         public UnorderedPair<PhysicsObject> objects;
         Vector normal;
         double distance;
+        double time; // make the program go twice as fast
+        boolean valid;
 
         public Collision(UnorderedPair<PhysicsObject> objects) {
             this.objects = objects;
             normal = new Vector();
             distance = Double.NaN; // not yet calculated
+            time = Double.NaN;
+            valid = false;
         }
 
         public static Collision resolve(UnorderedPair<PhysicsObject> o) {
@@ -113,9 +161,18 @@ public class PhysicsController {
             if (B instanceof MovingObject) {
                 vB = ((MovingObject) B).getVelocity();
             }
+            if (!(A instanceof MovingObject) && B instanceof MovingObject) {
+                // swap them
+                Vector temp = vA;
+                vA = vB;
+                vB = temp;
+                col.objects = new UnorderedPair<>(B, A);
+                A = col.objects.getA();
+                B = col.objects.getB();
+            }
             Vector vel = vA.minus(vB);
-            AxisAlignedBoundingBox bA = o.getA().getAxisAlignedBoundingBox(),
-                    bB = o.getB().getAxisAlignedBoundingBox();
+            AxisAlignedBoundingBox bA = A.getAxisAlignedBoundingBox(),
+                    bB = B.getAxisAlignedBoundingBox();
             double xInvEntry, yInvEntry, xInvExit, yInvExit;
             if (vel.x > 0) {
                 xInvEntry = bB.x1 - bA.x2;
@@ -148,14 +205,32 @@ public class PhysicsController {
             }
             double entry = Math.max(xEntry, yEntry);
             double exit = Math.min(xExit, yExit);
-            if (entry > exit || xEntry < 0 && yEntry < 0 || xEntry > 1 || yEntry > 1) {
-                System.out.println("(entry > exit) = " + (entry > exit));
-                System.out.println("(xEntry < 0 && yEntry < 0) = " + (xEntry < 0 && yEntry < 0));
-                System.out.println("(xEntry > 1) = " + (xEntry > 1));
-                System.out.println("(yEntry > 1) = " + (yEntry > 1));
+            col.time = entry;
+            col.distance = entry * vel.magnitude();
+            if (entry > exit || xEntry > 1 || yEntry > 1) {
+                // no collision
                 return null;
             }
-            if (xEntry > yEntry) {
+            // this means they haven't already collided
+            if (xEntry > 0 || yEntry > 0) {
+                col.valid = true;
+            }
+            if (!col.valid) {
+                // find the closest distance
+                // also, fudge the numbers so that we can choose which
+                // side to put the normal on
+                if (Math.abs(bB.x1 - bA.x2) > Math.abs(bB.x2 - bA.x1)) {
+                    xInvEntry = bB.x2 - bA.x1 - 1;
+                } else {
+                    xInvEntry = bB.x1 - bA.x2 + 1;
+                }
+                if (Math.abs(bB.y1 - bA.y2) > Math.abs(bB.y2 - bA.y1)) {
+                    yInvEntry = bB.y2 - bA.y1 - 1;
+                } else {
+                    yInvEntry = bB.y1 - bA.y2 + 1;
+                }
+            }
+            if (col.valid ? xEntry > yEntry : Math.abs(xInvEntry) < Math.abs(yInvEntry)) {
                 if (xInvEntry < 0) {
                     col.normal.x = 1;
                     col.normal.y = 0;
@@ -172,7 +247,6 @@ public class PhysicsController {
                     col.normal.y = -1;
                 }
             }
-            col.distance = entry * vel.magnitude();
             return col;
         }
     }
