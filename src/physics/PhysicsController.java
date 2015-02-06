260deb6b3adc9696b767703f1f65c25c29b83369
firefly431 <firefly431.awesome@gmail.com>
2015-02-06 13:04:42 -0600

    Continue refactoring PhysicsController
    Requires that QuadTree be modified to use other AABBs passed in, and also that Collision.resolve() takes a maxTime parameter.

--- src/physics/PhysicsController.java
+++ src/physics/PhysicsController.java
@@ -6,6 +6,9 @@ import sherwood.gameScreen.GameScreen;
 import util.UnorderedPair;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PhysicsController {
     public static final double ELASTICITY = 0.9;
@@ -20,15 +23,39 @@ public class PhysicsController {
     }
 
     public void step() {
-        PriorityQueue<Collision> collisions = new PriorityQueue<>(new Comparator<Collision>() {
-            @Override
-            public int compare(Collision a, Collision b) {
-                return Double.compare(a.time, b.time);
+        Comparator<Collision> timeComparator = (Collision a, Collision b) -> Double.compare(a.time, b.time);;
+        PriorityQueue<Collision> collisions = new PriorityQueue<>(timeComparator);
+        HashMap<PhysicsObject, Double> time = new HashMap<>();
+        QuadTree quadTree = new QuadTree(new AxisAlignedBoundingBox(0, 0, GameScreen.WIDTH, GameScreen.HEIGHT));
+        // initialize the collisions PriorityQueue
+        collisions.addAll(quadTree.getAllCollidingPairs().stream().map(Collision::resolve).collect(Collectors.toList()));
+        while (!collisions.isEmpty()) {
+            Collision collision = collisions.remove();
+            // move both objects
+            if (collision.objects.getA() instanceof MovingObject) {
+                collision.objects.getA().addPosition(((MovingObject) collision.objects.getA()).getVelocity().times(collision.time));
             }
-        });
-        HashMap<PhysicsObject, Double> times = new HashMap<>();
-        HashSet<PhysicsObject> invalid = new HashSet<>();
-        //
+            if (collision.objects.getB() instanceof MovingObject) {
+                collision.objects.getB().addPosition(((MovingObject) collision.objects.getB()).getVelocity().times(collision.time));
+            }
+            if (collision.objects.getA() instanceof MovingObject) {
+                if (collision.objects.getB() instanceof MovingObject) {
+                    doCollisionMoving2((MovingObject) collision.objects.getA(), (MovingObject) collision.objects.getB(), collision);
+                } else {
+                    doCollisionMoving1((MovingObject) collision.objects.getA(), collision.objects.getB(), collision);
+                }
+            } else if (collision.objects.getB() instanceof MovingObject) {
+                doCollisionMoving1((MovingObject) collision.objects.getB(), collision.objects.getA(), collision);
+            }
+            // remove collisions involving either
+            PriorityQueue<Collision> newQueue = new PriorityQueue<>(timeComparator);
+            newQueue.addAll(collisions.stream().filter((Collision test) ->
+                    !(test.objects.getA() == collision.objects.getA() || test.objects.getB() == collision.objects.getA()
+                   || test.objects.getA() == collision.objects.getB() || test.objects.getB() == collision.objects.getB())
+            ).collect(Collectors.toList()));
+            // TODO: recalculate AABBs and add
+            // make sure to pass max time to Collision.resolve()
+        }
     }
 
     // moving and static
@@ -38,21 +65,14 @@ public class PhysicsController {
         b.collide(a);
         // TODO: real code
         System.out.println("Collision!");
-        a.setPosition(a.getPosition().plus(a.getVelocity().times(collision.time)));
         a.setVelocity(new Vector());
     }
 
     // moving and moving
     private void doCollisionMoving2(MovingObject a, MovingObject b,
                                     Collision collision) {
-
         a.collide(b);
         b.collide(a);
-
-        // move to collision
-        a.setPosition(a.getPosition().plus(a.velocity.times(collision.time)));
-        b.setPosition(b.getPosition().plus(b.velocity.times(collision.time)));
-
         if (a.velocity.dot(b.velocity) < 0)
             if (collision.normal.x == 0) {
                 a.setVelocity(new Vector(a.getVelocity().x, 0));
@@ -69,5 +89,4 @@ public class PhysicsController {
             b.setVelocity(new Vector(0, 0));
         }
     }
-
 }
